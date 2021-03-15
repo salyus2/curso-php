dac02a4f3068af663324e22924641e4057295909
Chisco <francisco.salcines@sudespacho.net>
2021-03-15 09:37:18 +0100

    Login a medias

--- practicas/login_registro/views/contenido.view.php
+++ practicas/login_registro/views/contenido.view.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link href='https://fonts.googleapis.com/css?family=Raleway:400,300' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/estilos.css">
 
     <title>Contenido</title>
@@ -16,6 +16,8 @@
     <div class="contendor">
         <h1 class="titulo">Contenido del sitio</h1>
         <a href="cerrar.php">Cerrar Sesión</a>
+        <hr class="border">
+        
         <div class="contenido">
         <article>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste id ea aut cupiditate tempore, similique quas suscipit. Fugiat ipsam similique, ut sit tempore id culpa ad! Excepturi non quia iste.</p>
---
+++ practicas/login_registro/views/registrate.view.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link href='https://fonts.googleapis.com/css?family=Raleway:400,300' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/estilos.css">
+
+    <title>Registrate</title>
+</head>
+
+<body>
+    <div class="contendor">
+        <h1 class="titulo">Registrate</h1>
+        <hr class="border">
+
+        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST" , class="formulario" , name="login">
+            <div class="form-group">
+                <i class="icono izquierda fa fa-user"></i><input type="text" , name="usuario" class="usuario" placeholder="Usuario">
+            </div>
+            <div class="form-group">
+                <i class="icono izquierda fa fa-lock"></i><input type="password" , name="password" class="password" placeholder="Contraseña">
+            </div>
+            <div class="form-group">
+                <i class="icono izquierda fa fa-user"></i><input type="text" , name="usuario" class="usuario" placeholder="Usuario">
+            </div>
+        </form>
+    </div>
+    </div>
+</body>
+
+</html>
